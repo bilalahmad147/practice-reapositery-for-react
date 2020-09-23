53d5c0c0b5516fed5fae9afca63b7f8d397f1e95
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-23 16:47:13 +0500

    updated two files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./App.css";
+import Child from "./Child";
 
 function App() {
 
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Child />
     </div>
   );
 }
