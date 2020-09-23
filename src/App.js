f0fbbbce7d39e722988c24f170d73d725a6bdbac
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-23 16:14:05 +0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import "./App.css";
-import Balance from "./Components/Balance";
-import ExpenseDetail from "./Components/ExpenseDetail";
-import History from "./Components/History";
-import Expense from "./Components/Expense";
-import { TransProvider } from "./Components/ExpenseContext";
 
 function App() {
 
   return (
-    <TransProvider>
-      <div className="App">
-        <Balance />
-        <ExpenseDetail />
-        <h2>HISTORY</h2>
-        <History />
-        <Expense />
-      </div>
-    </TransProvider>
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
   );
 }
 
