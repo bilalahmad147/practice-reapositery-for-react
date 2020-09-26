0281569fac123e62aadf6643a80dabbf44bb42a4
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-26 19:32:30 +0500

    updated app.js and counter.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './Counter';
 
 function App() {
-  let initialValue = 0;
-  let [isCount, setCount] = useState(initialValue);
-
-  const formatCount = () => {
-    return isCount === initialValue ? "Zero" : isCount;
-  };
-
-  let classes = "badge m-2 badge-";
-  classes += (isCount === 0) ? "warning" : "primary";
-
-
 
   return (
     <React.Fragment>
-      <h1>{isCount}</h1>
-      <span style={{ fontSize: 20, fontWeight: "bold" }} className={classes}>{formatCount()}</span>
-      <button onClick={() => { setCount(++isCount) }} className="btn btn-secondary btn-sm m-2">INCREMENT</button>
-      <button onClick={() => { setCount(--isCount) }} className="btn btn-secondary btn-sm m-2">DECREMENT</button>
-      <button onClick={() => { setCount(initialValue) }} className="btn btn-secondary btn-sm m-2">RESET</button>
+      <Counter />
+      <Counter />
     </React.Fragment>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  let initialValue = 0;
+  let [isCount, setCount] = useState(initialValue);
+
+  const formatCount = () => {
+    return isCount === initialValue ? "Zero" : isCount;
+  };
+
+  let classes = "badge m-2 badge-";
+  classes += (isCount === 0) ? "warning" : "primary";
+
+
+
+  return (
+    <React.Fragment>
+      <h1>{isCount}</h1>
+      <span style={{ fontSize: 20, fontWeight: "bold" }} className={classes}>{formatCount()}</span>
+      <button onClick={() => { setCount(++isCount) }} className="btn btn-secondary btn-sm m-2">INCREMENT</button>
+      <button onClick={() => { setCount(--isCount) }} className="btn btn-secondary btn-sm m-2">DECREMENT</button>
+      <button onClick={() => { setCount(initialValue) }} className="btn btn-secondary btn-sm m-2">RESET</button>
+    </React.Fragment>
+  );
+}
+
+export default Counter;
