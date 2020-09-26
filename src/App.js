c7bb097d77c13ffe0d5a6cff305548d76bd22e5c
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-26 16:25:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   let initialValue = 0;
+  let [isCount, setCount] = useState(initialValue);
 
   const formatCount = () => {
-    return initialValue === 0 ? "Zero" : initialValue;
+    return isCount === initialValue ? "Zero" : isCount;
   };
 
   let classes = "badge m-2 badge-";
-  classes += (initialValue === 0) ? "warning" : "primary"
+  classes += (isCount === 0) ? "warning" : "primary";
+
+
 
   return (
     <React.Fragment>
-      <span style={{fontSize : 20}} className={classes}>{formatCount()}</span>
-      <button className="btn btn-secondary btn-sm m-2">+</button>
-      <button className="btn btn-secondary btn-sm m-2">-</button>
-      <button className="btn btn-secondary btn-sm m-2">DELETE</button>
+      <h1>{isCount}</h1>
+      <span style={{ fontSize: 20, fontWeight: "bold" }} className={classes}>{formatCount()}</span>
+      <button onClick={() => { setCount(++isCount) }} className="btn btn-secondary btn-sm m-2">INCREMENT</button>
+      <button onClick={() => { setCount(--isCount) }} className="btn btn-secondary btn-sm m-2">DECREMENT</button>
+      <button onClick={() => { setCount(initialValue) }} className="btn btn-secondary btn-sm m-2">RESET</button>
     </React.Fragment>
   );
 }
