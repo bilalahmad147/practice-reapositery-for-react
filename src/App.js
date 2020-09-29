5ae38d348f686c33724bba8bb7de87abb1987ae6
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-29 16:08:10 +0500

    basic react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Counter from './Counter';
-import Counter1 from './Counter1';
-import CounterContext from "./createContext";
 
 
 function App() {
 
-  const countState = useState(0);
-
   return (
-    <CounterContext.Provider value={countState}>
       <React.Fragment>
-        <h1>{countState}</h1>
-        <Counter />
-        <br />
-        <Counter1 />
+        <h1>hello world</h1>
       </React.Fragment>
-    </CounterContext.Provider>
   );
 }
 
