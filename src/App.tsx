c58e379ca8674a080f2e5864dae0de1ac455a657
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-05-22 16:40:29 +0500

    updated changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import TodoListItem from "./todoListItem";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+const toggleTodo = (selectedTodo: Todo) => {
+  const newTodos = todos.map(todo => {
+    if (todo === selectedTodo) {
+      return {
+        ...todo,
+        complete: !todo.complete,
+      };
+    }
+    return todo;
+  });
+  setTodos(newTodos);
+};
+
+return (
+  <ul>
+    <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+    <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
+  </ul>
+);
 }
 
 export default App;
